8ec4c1c70d626f2037d929d4670649a4776e818c
vatsaa803 <140444561+vatsaa803@users.noreply.github.com>
2025-09-21 07:03:46 -0700

    Create q8.php

---
+++ Assignment 8/q8.php
@@ -0,0 +1,7 @@
+<?php
+$age=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
+asort($age); print_r($age);
+ksort($age); print_r($age);
+arsort($age); print_r($age);
+krsort($age); print_r($age);
+?>
